4701b4ba8ae851d4f2eaeff40d8fb6167c7cec5c
Massimo Nocentini <massimo.nocentini@gmail.com>
2022-10-18 09:59:10 +0200

    Also ported the goto version

--- src/dl.lua
+++ src/dl.lua
@@ -73,6 +73,52 @@ function dl.solver (llink, rlink, ulink, dlink, len, top, option, primary_header
 		end
 	end
 
+	local function G (l, options)
+
+		::x1::
+		local item, ref = nil, {}	-- to silent the interpreter on goto checks.
+
+		::x2::	
+		if iscovered () then 
+			local cpy = {} 
+			for k, v in pairs(options) do cpy[k] = v end
+			table.sort(cpy)
+			coroutine.yield (cpy)
+			goto x8 
+		end
+
+		::x3::
+		item = rlink[primary_header]		-- just pick the next item to be covered.
+		assert(primary_header ~= item)		-- which hasn't to be the `primary_header`.
+
+		::x4::
+		cover (item)
+		ref[l] = dlink[item]
+		options[l] = option[ref[l]]
+
+		::x5::
+		if ref[l] == item then goto x7
+		else
+			loop (ref[l], rlink, function (p) cover(top[p]) end)
+			l = l + 1; goto x2
+		end
+
+		::x6::
+		loop (ref[l], llink, function (p) uncover(top[p]) end)
+		item = top[ref[l]]
+		ref[l] = dlink[ref[l]]
+		goto x5
+
+		::x7::
+		uncover (item)
+		
+		::x8::
+		options[l] = nil	-- cleaning it up for the next solution.
+		item, ref[l] = nil, nil
+		if l > 1 then l = l - 1; goto x6 end
+
+	end
+
 	return coroutine.create (function () 
 		local options = {}
 		R (1, options) 
