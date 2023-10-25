9d54c978f6929836e5b065884d9ba24bcc2a1abe
Massimo Nocentini <massimo.nocentini@gmail.com>
2023-10-25 15:59:29 +0200

    Working shared library.

---
+++ knuth/src/dlx1/dlx.c
@@ -0,0 +1,10 @@
+
+#include <stdio.h>
+#include <dlx1.h>
+
+int main(int argc, char *argv[])
+{
+    dlx1(argc, argv);
+
+    return 0;
+}
---
+++ knuth/src/dlx1/dlx1.h
@@ -0,0 +1,5 @@
+
+
+#include <stdio.h>
+
+void dlx1(int argc, char *argv[]);
